b19aa0a96582af60a4681899a7c43f7bdd2c58d0
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-01-10 22:26:28 +0100

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <div>
+        <h2>Emmanuel Fatomoye</h2>
+        <p>Front-end Web Developer</p>
+      </div>
+      <div>
+        <ul>
+          <li>
+              
+          </li>
+          <li>
+
+          </li>
+          <li>
+
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Header
